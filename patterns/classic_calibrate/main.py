6dca8e2405536ec0abf95c4afe6094e2318b53ed
ChristofferNorgaard <50552689+ChristofferNorgaard@users.noreply.github.com>
2024-12-05 19:25:45 +0100

    fixed classic calibrate

--- patterns/classic_calibrate/main.py
+++ patterns/classic_calibrate/main.py
@@ -3,13 +3,13 @@ import time
 print('#{"version": 0, "led_count": 500, "fps": 60}')
 green = "#" + "00ff00" * 500
 red = "#" + "ff0000" * 500
-normal = "000000" * 500
-print(red)
+print(red, flush=True)
 time.sleep(0.1)
 for i in range(500):
-    print(green)
+    print(green, flush=True)
     time.sleep(0.1)
     cur = "#" + "000000"*i + "ffffff" + "000000"*(500-1-i)
+    print(cur, flush=True)
     time.sleep(0.1)
 
 time.sleep(0.1)
