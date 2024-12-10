93ff740602c28c28f964b1ccc2c8b2a21e3fd3d9
ChristofferNorgaard <50552689+ChristofferNorgaard@users.noreply.github.com>
2024-12-10 11:37:09 +0100

    xyz clearing

--- patterns/xyz-debug/main.py
+++ patterns/xyz-debug/main.py
@@ -9,6 +9,7 @@ with open("3Dlocs.csv") as f:
         lucke.append([int(l[0]), float(l[1]), float(l[2]), float(l[3])])
 state = ["000000"]*500
 while True:
+    state = ["000000"]*500
     print("#" + ''.join(state), flush=True)
     for lucka in sorted(lucke, key= lambda x: x[1]):
        state[lucka[0]] = "ff0000"
@@ -18,6 +19,7 @@ while True:
     for lucka in sorted(lucke, key= lambda y: y[2]):
        state[lucka[0]] = "00ff00"
        print("#" + ''.join(state), flush=True)
+    state = ["000000"]*500
     print("#" + ''.join(state), flush=True)
     for lucka in sorted(lucke, key= lambda z: z[3]):
        state[lucka[0]] = "00ff00"
