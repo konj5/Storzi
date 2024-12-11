8eee1c0059ed53bf55e3f6b9f4a6b76bf1d4453b
Filip Štamcar <filip.stamcar@hotmail.com>
2024-12-11 23:29:10 +0100

    Add more patterns and update existing ones
    Co-authored-by: Bor Brudar <bor.brudar@gmail.com>

--- patterns/spiral/main.py
+++ patterns/spiral/main.py
@@ -1,48 +1,58 @@
-from jelka import Jelka, Color
-from jelka.sphere import Sphere
 import math
+from typing import List
+
+from jelka import Jelka
+from jelka.shapes import Sphere
+from jelka.types import Color, Position
+
+normalized: List[Position]
+color: Color
 
 
 def init(jelka: Jelka):
     global normalized
-    normalized = [0] * jelka.n
 
-    min_x = min([jelka.positions_raw[i][0] for i in range(jelka.n)])
-    max_x = max([jelka.positions_raw[i][0] for i in range(jelka.n)])
-    min_y = min([jelka.positions_raw[i][1] for i in range(jelka.n)])
-    max_y = max([jelka.positions_raw[i][1] for i in range(jelka.n)])
-    min_z = min([jelka.positions_raw[i][2] for i in range(jelka.n)])
-    max_z = max([jelka.positions_raw[i][2] for i in range(jelka.n)])
-    for i in range(len(normalized)):
-        normalized[i] = (
-            (jelka.positions_raw[i][0] - min_x) / (max_x - min_x + 0.01),
-            (jelka.positions_raw[i][1] - min_y) / (max_y - min_y + 0.01),
-            (jelka.positions_raw[i][2] - min_z) / (max_z - min_z + 0.01),
+    normalized = [Position(0, 0, 0)] * jelka.number_of_lights
+
+    min_x = min([pos.x for pos in jelka.positions_raw.values()])
+    max_x = max([pos.x for pos in jelka.positions_raw.values()])
+    min_y = min([pos.y for pos in jelka.positions_raw.values()])
+    max_y = max([pos.y for pos in jelka.positions_raw.values()])
+    min_z = min([pos.z for pos in jelka.positions_raw.values()])
+    max_z = max([pos.z for pos in jelka.positions_raw.values()])
+
+    for light, position in jelka.positions_raw.items():
+        normalized[light] = Position(
+            (position.x - min_x) / (max_x - min_x + 0.01),
+            (position.y - min_y) / (max_y - min_y + 0.01),
+            (position.z - min_z) / (max_z - min_z + 0.01),
         )
 
 
 def callback(jelka: Jelka):
-    global col
+    global color
 
     height = 1 - 0.0075 * (jelka.frame % 150)
+
     if height == 1:
-        col = Color.vivid(Color.random_color())
+        color = Color.vivid(Color.random())
+
     rad = 1 / 2 - height / 2
     x = 0.5 + rad * math.cos(height * 20)
     y = 0.5 + rad * math.sin(height * 20)
 
-    sph = Sphere((x, y, height), 0.1)
+    sphere = Sphere((x, y, height), 0.1)
 
     for i in range(len(jelka.lights)):
-        pos = normalized[i]
-        if sph.is_inside(pos):
-            jelka.set_light(i, col)
+        position = normalized[i]
+        if sphere.is_inside(position):
+            jelka.set_light(i, color)
         # else:
         #   jelka.set_light(i, Color(0, 0, 0))
 
 
 def main():
-    jelka = Jelka(500, 60)
+    jelka = Jelka(60)
     jelka.run(callback, init)
 
 
