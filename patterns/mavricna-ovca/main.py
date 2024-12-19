653653be41eb12caf8887213719922c58cfa4ec7
vida26365 <vm36265@student.uni-lj.si>
2024-12-19 16:18:04 +0100

    Fix linter and format

--- patterns/mavricna-ovca/main.py
+++ patterns/mavricna-ovca/main.py
@@ -1,5 +1,3 @@
-import math
-
 from jelka import Jelka
 from jelka.types import Color
 
@@ -11,11 +9,8 @@ def callback(jelka: Jelka):
             Color(
                 ((position[2] * 10) * jelka.elapsed_time * 1) % 256,
                 ((position[2] * 10) * jelka.elapsed_time * 4) % 256,
-                ((position[2] * 10) * jelka.elapsed_time * 2) % 256
-                
-                #position[0] * 255 + math.sin((jelka.elapsed_time + 1) / 4) * 255 + 256) % 256,
-                
-            )
+                ((position[2] * 10) * jelka.elapsed_time * 2) % 256,
+            ),
         )
 
 
