7b7d57f117e4c9d2da73392eb3acbf182de57f63
Filip Štamcar <filip.stamcar@hotmail.com>
2025-12-17 19:18:48 +0100

    Remove outdated patterns

--- patterns/debug-pattern-1/main.py
+++
@@ -1,9 +0,0 @@
-import time
-
-print('#{"version": 0, "led_count": 500, "fps": 60}')
-
-while True:
-    print("#" + "ff0000" * 500)
-    time.sleep(1)
-    print("#" + "0000ff" * 500)
-    time.sleep(1)
--- patterns/debug-pattern-2/main.py
+++
@@ -1,9 +0,0 @@
-import time
-
-print('#{"version": 0, "led_count": 500, "fps": 60}')
-
-while True:
-    print("#" + "00ff00" * 500)
-    time.sleep(1)
-    print("#" + "ff00ff" * 500)
-    time.sleep(1)
